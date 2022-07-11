e94f4b009a4e53f49b5e9ce3a9d5ab3593dfd561
MOWLID OMAR <107882442+LeedJaango23@users.noreply.github.com>
2022-07-11 08:59:48 -0500

    Add files via upload

---
+++ about.js
@@ -0,0 +1,19 @@
+console.log("hello world");
+
+
+
+function handleSubmit(evt) {
+	evt.preventDefault();
+	
+	console.log('form submit');
+}
+
+
+let form = document.querySelector('#contact');
+
+form.addEventListener('submit', handleSubmit);
+
+form.addEventListener('mouseover', (event) => {});
+
+onmouseover = (event) => { };
+
---
+++ profile.js
@@ -0,0 +1,6 @@
+btns = document. getElementsByClassName("saveBtn");
+for (let i = 0; i < btns. length; i++) {
+btns[i]. addEventListener("click", function () {
+
+});
+}
